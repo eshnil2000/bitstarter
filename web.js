0b656f03028d9b03f85e00bdf88130d17cb85d9d
Ubuntu <ubuntu@ip-10-253-93-62.us-west-2.compute.internal>
2013-07-06 16:05:23 +0000

    Print from file

--- web.js
+++ web.js
@@ -1,9 +1,22 @@
 var express = require('express');
-
+var fs = require('fs');
 var app = express.createServer(express.logger());
+buf = new Buffer(256);
+
+buf=fs.readFileSync('index.html', 'utf8');
+//function (err, data) 
+//{
+//  if (err) throw err;
+//  len = buf.write(data,0);
+//console.log("length" + len);  
+console.log(buf.toString());
+  
+//  console.log(data);
+//});
 
 app.get('/', function(request, response) {
-  response.send('Hello World2 !');
+//  response.send('Hello World2 !');
+response.send(buf.toString());
 });
 
 var port = process.env.PORT || 5000;
